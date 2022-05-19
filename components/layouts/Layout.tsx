abeea32b71728bdfb95e3cbc81a8471b00e0a100
danieldamian09 <humbertorivero9@gmail.com>
2022-05-19 16:08:51 -0300

    Add layout and navbar

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,7 +1,7 @@
 import {FC} from "react";
 import Head from "next/head";
 import {Box} from "@mui/system";
-import React from "react";
+import {Navbar} from '../ui';
 
 interface Props {
 	title?: string;
@@ -14,7 +14,7 @@ export const Layout: FC<Props> = ({title = "OpenJira - App", children}) => {
 			<Head>
 				<title>{title}</title>
 			</Head>
-			{/* Navbar */}
+      <Navbar />
 
 			{/* Sidebar */}
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,15 @@
-import { Typography } from '@mui/material';
+import {Typography} from "@mui/material";
 import type {NextPage} from "next";
+import {Layout} from "../components/layouts";
 
 const HomePage: NextPage = () => {
-	return <Typography variant='h1' color="primary">Hola Mundo</Typography>;
+	return (
+		<Layout>
+			<Typography variant="h1" color="primary">
+				Hola Mundo
+			</Typography>
+		</Layout>
+	);
 };
 
 export default HomePage;
