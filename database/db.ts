5b753d3cdbee57745888171b48724efacca04f83
danieldamian09 <humbertorivero9@gmail.com>
2022-06-03 19:25:04 -0300

    Add conect to MongoDB with Mongoose

---
+++ database/db.ts
@@ -0,0 +1,39 @@
+import mongoose, { mongo } from 'mongoose';
+/*
+  0 = disconnected
+  1 = connected
+  2 = connecting
+  3 = disconnecting
+*/
+
+const mongooConnection = {
+  isConnected: 0,
+}
+
+export const connect = async() => {
+  if (mongooConnection.isConnected) {
+    console.log('Ya esta conectado');
+    return;
+  }
+
+  if (mongoose.connections.length > 0) {
+    mongooConnection.isConnected = mongoose.connections[0].readyState;
+
+    if (mongooConnection.isConnected === 1) {
+      console.log('Usando conexion anterior');
+      return;
+    }
+    await mongoose.disconnect();
+  }
+
+  await mongoose.connect('....')
+  mongooConnection.isConnected = 1;
+  console.log('Contectado a MongoDB', '.....');
+}
+
+export const disconnect = async () => {
+  if (mongooConnection.isConnected !== 0) return;
+  
+  await mongoose.disconnect();
+  console.log('Desconectado de MongoDB', '.....');
+}
