ce48402dad86da008d95fc8011133ff762b74309
danieldamian09 <humbertorivero9@gmail.com>
2022-06-07 11:11:34 -0300

    Feat connect MongoDB from Restful API

--- database/db.ts
+++ database/db.ts
@@ -12,7 +12,7 @@ const mongooConnection = {
 
 export const connect = async() => {
   if (mongooConnection.isConnected) {
-    console.log('Ya esta conectado');
+    console.log('Ya estabamos conectado');
     return;
   }
 
@@ -26,14 +26,14 @@ export const connect = async() => {
     await mongoose.disconnect();
   }
 
-  await mongoose.connect('....')
+  await mongoose.connect(process.env.MONGO_URL || "")
   mongooConnection.isConnected = 1;
-  console.log('Contectado a MongoDB', '.....');
+  console.log('Contectado a MongoDB', process.env.MONGO_URL);
 }
 
 export const disconnect = async () => {
-  if (mongooConnection.isConnected !== 0) return;
+  if (mongooConnection.isConnected === 0) return;
   
   await mongoose.disconnect();
-  console.log('Desconectado de MongoDB', '.....');
+  console.log('Desconectado de MongoDB');
 }
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,4 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Console } from 'console'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
@@ -11,6 +12,9 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+
+  console.log(process.env)
+
   res.status(200).json({
     ok: true,
     message: 'Todo correcto',
