a864b0a307c987ce53c5480008bc50d920930f5f
danieldamian09 <humbertorivero9@gmail.com>
2022-06-16 11:31:04 -0300

    Feat edit and add new entry from page entry id

--- pages/entries/[id].tsx
+++ pages/entries/[id].tsx
@@ -1,6 +1,25 @@
-import { Button, Card, CardActions, CardContent, CardHeader, Grid, TextField } from "@mui/material";
-import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined';
+import {
+  capitalize,
+	Button,
+	Card,
+	CardActions,
+	CardContent,
+	CardHeader,
+	FormControl,
+	FormControlLabel,
+	FormLabel,
+	Grid,
+	Radio,
+	RadioGroup,
+	TextField,
+  IconButton,
+} from "@mui/material";
+import SaveOutlinedIcon from "@mui/icons-material/SaveOutlined";
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 import {Layout} from "../../components/layouts";
+import {EntryStatus} from "../../interfaces";
+
+const validStatus: EntryStatus[] = ["pending", "in-progress", "finished"];
 
 export const EntryPage = () => {
 	return (
@@ -11,32 +30,55 @@ export const EntryPage = () => {
 						<CardHeader
 							title="Entrda:"
 							subheader={`Creada hace: .... minutos`}
-            />
-            <CardContent>
-              <TextField
-                sx={{ marginBottom: 1, marginTop: 2 }}
-                fullWidth
-                placeholder='Nueva Entrada'
-                autoFocus
-                multiline
-                label='Nueva Entrada'
-              />
+						/>
+						<CardContent>
+							<TextField
+								sx={{marginBottom: 1, marginTop: 2}}
+								fullWidth
+								placeholder="Nueva Entrada"
+								autoFocus
+								multiline
+								label="Nueva Entrada"
+							/>
 
-              {/* TODO: Radio */}
-
-            </CardContent>
-            <CardActions>
-              <Button
-                startIcon={<SaveOutlinedIcon />}
-                variant="contained"
-                fullWidth
-              >
-                Save
-              </Button>
-            </CardActions>
+							<FormControl>
+								<FormLabel>Estado:</FormLabel>
+                <RadioGroup
+                  row
+                >
+									{validStatus.map((option) => (
+										<FormControlLabel
+											key={option}
+											value={option}
+											control={<Radio />}
+											label={capitalize(option)}
+										/>
+									))}
+								</RadioGroup>
+							</FormControl>
+						</CardContent>
+						<CardActions>
+							<Button
+								startIcon={<SaveOutlinedIcon />}
+								variant="contained"
+								fullWidth
+							>
+								Save
+							</Button>
+						</CardActions>
 					</Card>
 				</Grid>
-			</Grid>
+      </Grid>
+      <IconButton
+        sx={{
+          position: 'fixed',
+          bottom: 30,
+          right: 30,
+          backgroundColor: 'error.dark',
+        }}
+      >
+        <DeleteOutlineOutlinedIcon />
+      </IconButton>
 		</Layout>
 	);
 };
